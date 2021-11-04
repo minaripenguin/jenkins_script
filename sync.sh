d5ed0b27d8fbd0ee57c721f2765651fc9cf06da0
IU <alexfinhart@gmail.com>
2021-11-04 22:18:09 +0800

    Update and rename sync.sh to build.sh

--- sync.sh
+++
@@ -1,2 +0,0 @@
-repo init --depth=1 --no-repo-verify -u git://github.com/P-Arcana/Arcana_manifest -b S -g default,-device,-mips,-darwin,-notdefault
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
